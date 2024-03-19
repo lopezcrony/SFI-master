c9424af258523ff44729ac5d75c9eeb236a89c96
Dm29015 <dmcorrea670@soy.sena.edu.co>
2024-03-18 19:03:51 -0500

    Parciales y rutas

--- main.js
+++ main.js
@@ -1,22 +1,74 @@
 const express = require('express')
+const hbs = require('hbs')
+
 const app = express()
 const port = 3000
 
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) { console.log(err); });
+
 app.use(express.static('public'))
 
-  app.get('/sales', (req, res) => {
-    res.sendFile(__dirname+'/public/sales.html')
-  })
+// --------------------------------------URL----------------------------------------------
+app.get('/', (req, res) => {
+  res.render('index')
+});
 
-  app.get('/newSales', (req, res) => {
-    res.sendFile(__dirname+'/public/newSale.html')
-  })
+app.get('/home', (req, res) => {
+  res.render('home')
+});
 
+app.get('/company', (req, res) => {
+  res.render('company')
+});
 
-  app.get('*', (req, res) => {
-    res.send('404| Paila mano')
-  })
+app.get('/providers', (req, res) => {
+  res.render('providers')
+});
+
+app.get('/payments', (req, res) => {
+  res.render('payments')
+});
+
+app.get('/categories', (req, res) => {
+  res.render('categories')
+});
+
+app.get('/admin', (req, res) => {
+  res.render('admin')
+});
+
+app.get('/client', (req, res) => {
+  res.render('client')
+});
+
+app.get('/categories', (req, res) => {
+  res.render('categories')
+});
+
+app.get('/products', (req, res) => {
+  res.render('products')
+})
+
+app.get('/sales', (req, res) => {
+  res.render('sales')
+})
+
+app.get('/newSale', (req, res) => {
+  res.render('newSale')
+})
+
+app.get('/inventory', (req, res) => {
+  res.render('inventory')
+});
+
+app.get('*', (req, res) => {
+  res.send('404| Paila mano')
+})
+
+// ---------------------------------------------------------------------------------------
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`http://localhost:${port}`)
+});
